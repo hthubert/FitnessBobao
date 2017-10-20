d6b12a9970791cf460ddbeccbf438ac02a6f5390
hetao <yechaht@gmail.com>
2017-10-20 14:43:00 +0800

    完成生成语音文件的功能实验

--- tools/BaiduTextToVoice/Program.cs
+++ tools/BaiduTextToVoice/Program.cs
@@ -23,10 +23,10 @@ namespace BaiduTextToVoice
             const int amplitude = 0;
             var numSamples = (uint)(format.SampleRate * format.Channels);
             var buffer = new short[numSamples];
-            var t = (Math.PI * 2 * (format.SampleRate / 100.0)) / (format.AverageBytesPerSecond * format.Channels);            
+            var t = (Math.PI * 2 * (format.SampleRate / 100.0)) / (format.AverageBytesPerSecond * format.Channels);
             for (var i = 0; i < buffer.Length; i++) {
                 for (var channel = 0; channel < format.Channels; channel++) {
-                    buffer[i+channel] = Convert.ToInt16(amplitude * Math.Sin(t * i));
+                    buffer[i + channel] = Convert.ToInt16(amplitude * Math.Sin(t * i));
                 }
             }
             return null;
@@ -37,6 +37,29 @@ namespace BaiduTextToVoice
             return new byte[format.AverageBytesPerSecond * seconds];
         }
 
+        private static Stream WriteSpeech(byte[] speech, int repeat, ref WaveFileWriter writer)
+        {
+            const int readSize = 1024 * 4;
+            Stream output = null;
+            using (var stream = new MemoryStream(speech))
+            using (var reader = new Mp3FileReader(stream)) {
+                if (writer == null) {
+                    output = new MemoryStream();
+                    writer = new WaveFileWriter(output, reader.WaveFormat);
+                }
+                var seconds = (int)Math.Ceiling(reader.TotalTime.TotalSeconds);
+                var bytes = CreateSilentSound(reader.WaveFormat, seconds);
+                var index = 0;
+                while (reader.Read(bytes, index, readSize) == readSize) {
+                    index += readSize;
+                }
+                for (var i = 0; i < repeat; i++) {
+                    writer.Write(bytes, 0, bytes.Length);
+                }
+            }
+            return output;
+        }
+
         static void Main(string[] args)
         {
             var option = new Dictionary<string, object>()
@@ -45,21 +68,26 @@ namespace BaiduTextToVoice
                 {"vol", 5}, // 音量
                 {"per", 0}  // 发音人，4：情感度丫丫童声
             };
-            var result = _ttsClient.Synthesis("原地高抬腿", option);
-            if (!result.Success) {
-                Console.WriteLine(result.ErrorMsg);
-                Console.ReadLine();
-                return;
+            var text = "丁,10,9,8,7,6,5,4,3,2,1";
+            WaveFileWriter writer = null;
+            Stream output = null;
+            foreach (var s in text.Split(',')) {
+                var result = _ttsClient.Synthesis(s, option);
+                if (!result.Success) {
+                    Console.WriteLine(result.ErrorMsg);
+                }
+                var ret = WriteSpeech(result.Data, 1, ref writer);
+                if (ret != null) {
+                    output = ret;
+                }
             }
-            using (var stream = new MemoryStream(result.Data))
-            using (var reader = new Mp3FileReader(stream)) {
-                //reader.WaveFormat
-                //CreateOneSecondSilentSound(reader.WaveFormat);
-                var seconds = Math.Ceiling(reader.TotalTime.TotalSeconds);
-                //var 
-                //reader.Read()
+            writer?.Flush();
+            output?.Seek(0, SeekOrigin.Begin);
+            using (var fs = new FileStream("test.wav", FileMode.Create)) {
+                output?.CopyTo(fs);
+                fs.Close();
             }
-
+            writer?.Close();
             Console.ReadLine();
         }
 
